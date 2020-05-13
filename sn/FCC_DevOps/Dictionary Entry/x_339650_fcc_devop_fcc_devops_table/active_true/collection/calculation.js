240f661eaf18dc3696ba593c8c24dfe2acee1834
jayakumarss <jayakumar.sengodan@gmail.com>
2020-05-13 19:54:33 -0400

    'master' branch updated from  https://dev64721.service-now.com no-cicd

---
+++ sn/FCC_DevOps/Dictionary Entry/x_339650_fcc_devop_fcc_devops_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : FCC_DevOps
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-05-13 23:47:02
+ * Created By  : admin
+ * Updated On  : 2020-05-13 23:47:02
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=30e3a14ddb741010ab0a4870399619a5
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
